68f8c036d67870867278b7b0561a20fca3083e63
alesson.rodrigues <alesson.rodrigues@thegaragestudio.com.br>
2026-04-05 20:14:37 -0300

    feat: atalhos de teclado globais com modal de ajuda (? para abrir, G+letra para navegar)

--- CSharpAcademy.Web/src/app/shared/shared-module.ts
+++ CSharpAcademy.Web/src/app/shared/shared-module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { LevelUpOverlay } from './level-up-overlay/level-up-overlay';
 import { NotifBanner } from './notif-banner/notif-banner';
 import { ToastComponent } from './toast/toast';
 import { Pomodoro } from './pomodoro/pomodoro';
+import { Atalhos } from './atalhos/atalhos';
 
 @NgModule({
-  declarations: [LevelUpOverlay, NotifBanner, ToastComponent, Pomodoro],
-  imports: [CommonModule, FormsModule],
-  exports: [LevelUpOverlay, NotifBanner, ToastComponent, Pomodoro],
+  declarations: [LevelUpOverlay, NotifBanner, ToastComponent, Pomodoro, Atalhos],
+  imports: [CommonModule, FormsModule, RouterModule],
+  exports: [LevelUpOverlay, NotifBanner, ToastComponent, Pomodoro, Atalhos],
 })
 export class SharedModule {}
